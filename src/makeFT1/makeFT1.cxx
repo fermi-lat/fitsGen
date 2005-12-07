40d2a75dec583a355e9a890e5b012771314a0ad8
jchiang <>
2005-12-07 01:31:15 +0000

    use new template variables

--- src/makeFT1/makeFT1.cxx
+++ src/makeFT1/makeFT1.cxx
@@ -3,7 +3,7 @@
  * @brief Convert merit ntuple to FT1 format.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/src/makeFT1/makeFT1.cxx,v 1.4 2005/08/11 17:08:19 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/src/makeFT1/makeFT1.cxx,v 1.5 2005/12/04 20:39:08 mcenery Exp $
  */
 
 #include <iostream>
@@ -14,12 +14,11 @@
 #include "tip/Table.h"
 #include "tip/Header.h"
 
+#include "astro/SkyDir.h"
+
 #include "fitsGen/Util.h"
 
 using namespace fitsGen;
-
-void getEventFlags(tip::Table::Record & merit, short & isGamma, 
-                   short & goodPsf, short & goodEnergy);
 
 int main(int iargc, char * argv[]) {
    try {
@@ -52,35 +51,45 @@ int main(int iargc, char * argv[]) {
       int ncount(0);
       for ( ; merit_iter != meritTable->end(); ++merit_iter, ++ft1_iter) {
          ft1["energy"].set(merit["FT1Energy"].get());
-         ft1["ra"].set(merit["FT1Ra"].get());
-         ft1["dec"].set(merit["FT1Dec"].get());
+         double ra = merit["FT1Ra"].get();
+         double dec = merit["FT1Dec"].get();
+         ft1["ra"].set(ra);
+         ft1["dec"].set(dec);
+         astro::SkyDir dir(ra, dec);
+         ft1["l"].set(dir.l());
+         ft1["b"].set(dir.b());
          ft1["theta"].set(merit["FT1Theta"].get());
          ft1["phi"].set(merit["FT1Phi"].get());
          ft1["zenith_angle"].set(merit["FT1ZenithTheta"].get());
          ft1["earth_azimuth_angle"].set(merit["FT1EarthAzimuth"].get());
          ft1["time"].set(merit["EvtElapsedTime"].get());
          ft1["event_id"].set(static_cast<int>(merit["FT1EventId"].get()));
-         ft1["conversion_layer"].
-	   set(static_cast<short>(15-(merit["Tkr1FirstLayer"].get())));
-         ft1["imgoodcalprob"].set(merit["CTgoodCal"].get());
-         ft1["imvertexprob"].set(merit["CTvertex"].get());
-         ft1["imcoreprob"].set(merit["CTgoodPsf"].get());
-         ft1["impsferrpred"].set(merit["CTgoodPsf"].get());
-         ft1["calenergysum"].set(merit["CalEnergyRaw"].get());
-         ft1["caltotrln"].set(merit["CalTotRLn"].get());
-         ft1["imgammaprob"].set(merit["CTgamma"].get());
+         if (17 - merit["Tkr1FirstLayer"].get() < 11.5) { // Front converting
+            ft1["event_class"].set(0); 
+         } else { // Back converting
+            ft1["event_class"].set(1);
+         }
+//          ft1["conversion_layer"].
+// 	   set(static_cast<short>(15-(merit["Tkr1FirstLayer"].get())));
+//          ft1["imgoodcalprob"].set(merit["CTgoodCal"].get());
+//          ft1["imvertexprob"].set(merit["CTvertex"].get());
+//          ft1["imcoreprob"].set(merit["CTgoodPsf"].get());
+//          ft1["impsferrpred"].set(merit["CTgoodPsf"].get());
+//          ft1["calenergysum"].set(merit["CalEnergyRaw"].get());
+//          ft1["caltotrln"].set(merit["CalTotRLn"].get());
+//          ft1["imgammaprob"].set(merit["CTgamma"].get());
 
-         short isGamma, goodPsf, goodEnergy;
-         getEventFlags(merit, isGamma, goodPsf, goodEnergy);
-         tip::Table::Vector<short> calibVersion = ft1["calib_version"];
-         calibVersion[0] = isGamma;
-         calibVersion[1] = goodPsf;
-         calibVersion[2] = goodEnergy;
+//          short isGamma, goodPsf, goodEnergy;
+//          getEventFlags(merit, isGamma, goodPsf, goodEnergy);
+//          tip::Table::Vector<short> calibVersion = ft1["calib_version"];
+//          calibVersion[0] = isGamma;
+//          calibVersion[1] = goodPsf;
+//          calibVersion[2] = goodEnergy;
 
-         tip::Table::Vector<double> convPoint = ft1["conversion_point"];
-         convPoint[0] = merit["FT1ConvPointX"].get();
-         convPoint[1] = merit["FT1ConvPointY"].get();
-         convPoint[2] = merit["FT1ConvPointZ"].get();
+//          tip::Table::Vector<double> convPoint = ft1["conversion_point"];
+//          convPoint[0] = merit["FT1ConvPointX"].get();
+//          convPoint[1] = merit["FT1ConvPointY"].get();
+//          convPoint[2] = merit["FT1ConvPointZ"].get();
          ncount++;
       }
       std::cout << "number of rows processed: " << ncount << std::endl;
@@ -106,77 +115,5 @@ int main(int iargc, char * argv[]) {
       std::cout << eObj.what() << std::endl;
       return 1;
    }
-}
-
-void getEventFlags(tip::Table::Record & merit, short & isGamma, 
-                   short & goodPsf, short & goodEnergy) {
-   
-   isGamma = 0;
-   goodPsf = 0;
-   goodEnergy = 0;
-
-   //   bool good_energy_cut = merit["IMgoodCalProb"].get() > 0.2;
-   // bool zdir_cut = merit["Tkr1ZDir"].get() < -0.2;
-   // bool no_cal_cut = (merit["CalEnergySum"].get() < 5. 
-   //                    || merit["CalTotRLn"].get() < 2.);
-   //bool thin_cut = (merit["Tkr1FirstLayer"].get() != 0 
-   //                  && merit["Tkr1FirstLayer"].get() < 15);
-   //(void)(thin_cut);
-
-   //   bool global_cut = good_energy_cut && zdir_cut && !no_cal_cut;
-
-   //   bool psf_filter = (merit["IMcoreProb"].get() > 0.2 
-   //                  && merit["IMpsfErrPred"].get() < 3.);
-
-//    bool background_cut = (merit["TkrNumTracks"].get() > 0 
-//                           && merit["GltWord"].get() > 3
-//                           && merit["IMcoreProb"].get() > 0.2);
-//    bool veto(true);
-//    if (background_cut) {
-//       if (merit["VtxAngle"].get() > 0) {
-//          if (merit["EvtEnergySumOpt"].get() > 3500.) {
-//             veto = false;
-//          } else if (merit["EvtEnergySumOpt"].get() > 350.) {
-//             veto = false;
-//          } else {
-//             if (merit["Tkr1TotFirst"].get() > 4.5 
-//                 || merit["Tkr1TotAve"].get() > 3.5
-//                 || merit["AcdTotalEnergy"] > 0.25
-//                 || merit["VtxAngle"] > 0.4) {
-//                veto = true;
-//             } else {
-//                veto = false;
-//             }
-//          }
-//       } else {
-//          if (merit["EvtEnergySumOpt"].get() > 3500.) {
-//             veto = false;
-//          } else if (merit["EvtEnergySumOpt"].get() > 350.) {
-//             if (merit["Tkr1TotAve"] > 3. 
-//                 || merit["AcdTotalEnergy"] > 5.
-//                 || merit["EvtTkrEComptonRatio"] < 1) {
-//                veto = true;
-//             } else {
-//                veto = false;
-//             }
-//          } else {
-//             if (merit["Tkr1TotAve"] > 3. 
-//                 || merit["AcdTileCount"] > 0
-//                 || merit["AcdRibbonActDist"] > -300.
-//                 || merit["EvtTkrEComptonRatio"] < 1.05
-//                 || merit["FilterStatus_HI"] > 3.) {
-//                veto = true;
-//             } else {
-//                veto = false;
-//             }
-//          } // if EvtEnergySumOpt > 3500
-//       } // if VtxAngle > 0
-//    } // if background_cut
-//    if (!veto) isGamma = 1;
-
-// For DC1, IMgammaProb directly passes the background cut.
-//   if (merit["IMgammaProb"].get() > 0.5) isGamma = 1;
-//   if (psf_filter) goodPsf = 1;
-//   if (global_cut) goodEnergy = 1;
 }
 
