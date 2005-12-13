a4d33df2bc8ab64b413803fe08728f3596763a6c
jchiang <>
2005-12-13 00:06:40 +0000

    translate point_history tree in makeFT2

--- src/makeFT2/makeFT2.cxx
+++ src/makeFT2/makeFT2.cxx
@@ -3,7 +3,7 @@
  * @brief Convert Root D2 data from Gleam to FT2 format.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/src/makeFT2/makeFT2.cxx,v 1.3 2005/12/12 01:59:19 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/src/makeFT2/makeFT2.cxx,v 1.4 2005/12/12 21:12:48 jchiang Exp $
  */
 
 #include <iostream>
@@ -26,47 +26,37 @@ int main(int iargc, char * argv[]) {
 
       Util::getFileNames(iargc, argv, rootFile, fitsFile);
 
-      fitsGen::MeritFile pointing(rootFile, "Exposure");
+      fitsGen::MeritFile pointing(rootFile, "pointing_history");
       fitsGen::Ft2File ft2(fitsFile, pointing.nrows());
 
       for ( ; pointing.itor() != pointing.end(); pointing.next(), ft2.next()) {
-         ft2["start"].set(pointing["elapsed_time"]);
+         ft2["start"].set(pointing["start"]);
+         ft2["stop"].set(pointing["stop"]);
          tip::Table::Vector<float> scPosition = ft2["sc_position"];
-// @todo check units (FT2 specifies sc_position in m)
-         scPosition[0] = pointing["posx"];
-         scPosition[1] = pointing["posy"];
-         scPosition[2] = pointing["posz"];
-         ft2["lat_geo"].set(pointing["lat"]);
-         ft2["lon_geo"].set(pointing["lon"]);
-// @todo check units (FT2 specifies rad_geo in km)
-         ft2["rad_geo"].set(pointing["alt"]);
-         ft2["ra_scz"].set(pointing["raz"]);
-         ft2["dec_scz"].set(pointing["decz"]);
-         ft2["ra_scx"].set(pointing["rax"]);
-         ft2["dec_scx"].set(pointing["decx"]);
+         const tip::Table::Vector<float> merit_position =
+            const_cast<tip::TableCell&>(pointing.row()["sc_position"]);
+         scPosition[0] = merit_position[0];
+         scPosition[1] = merit_position[1];
+         scPosition[2] = merit_position[2];
+         ft2["lat_geo"].set(pointing["lat_geo"]);
+         ft2["lon_geo"].set(pointing["lon_geo"]);
+         ft2["rad_geo"].set(pointing["rad_geo"]*1e3); // convert to m
+         ft2["ra_zenith"].set(pointing["ra_zenith"]);
+         ft2["dec_zenith"].set(pointing["dec_zenith"]);
+         ft2["b_mcilwain"].set(pointing["B_McIlwain"]);
+         ft2["l_mcilwain"].set(pointing["L_McIlwain"]);
+         ft2["in_saa"].set(static_cast<bool>(pointing["in_saa"]));
+         ft2["ra_scz"].set(pointing["ra_scz"]);
+         ft2["dec_scz"].set(pointing["dec_scz"]);
+         ft2["ra_scx"].set(pointing["ra_scx"]);
+         ft2["dec_scx"].set(pointing["dec_scx"]);
+         ft2["livetime"].set(pointing["livetime"]);
       }
-
-// Get stop times. This is complicated since Table::Iterator is not
-// random access.
-      pointing.itor() = pointing.begin();
-      pointing.next();
       ft2.itor() = ft2.begin();
-      double start_time = ft2["start"].get();
-      for (; pointing.itor() != pointing.end();  pointing.next(), ft2.next()) {
-         ft2["stop"].set(pointing["elapsed_time"]);
-      }
-// Compute the last stop time based on the last interval of start times.
-      pointing.itor() = pointing.end();
-      pointing.prev();
-      pointing.prev();
-      double t1 = pointing["elapsed_time"];
-      pointing.next();
-      double t2 = pointing["elapsed_time"];
-      double stop_time = 2.*t2 - t1;
+      double start_time(ft2["start"].get());
       ft2.itor() = ft2.end();
       ft2.prev();
-      ft2["stop"].set(stop_time);
-
+      double stop_time(ft2["stop"].get());
       ft2.setObsTimes(start_time, stop_time);
    } catch (std::exception &eObj) {
       std::cout << eObj.what() << std::endl;
