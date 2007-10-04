8788692dd10ddd330f29774973cbffb9bf2b414f
jchiang <>
2007-10-04 20:41:04 +0000

    delete Mc* variables

--- python/Pass5_Classifier.py
+++ python/Pass5_Classifier.py
@@ -5,15 +5,12 @@ http://confluence.slac.stanford.edu/display/DC2/Summary+of+response+function+set
 @author J. Chiang <jchiang@slac.stanford.edu>
 """
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/python/Pass4_Classifier.py,v 1.3 2007/04/08 19:49:03 jchiang Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/python/Pass5_Classifier.py,v 1.1 2007/09/20 21:17:59 jchiang Exp $
 #
 from EventClassifier import EventClassifier
 
 meritVariables = """
 EvtRun    EvtEnergyCorr 
-McEnergy  McXDir  McYDir  McZDir   
-McXDirErr   McYDirErr  McZDirErr   
-McTkr1DirErr  McDirErr  
 GltWord   FilterStatus_HI 
 Tkr1FirstLayer  
 CTBBestEnergyProb CTBCORE CTBBestEnergy CTBBestEnergyRatio CTBClassLevel
