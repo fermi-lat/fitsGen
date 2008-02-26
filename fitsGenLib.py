0582d4310096d808a6343c97d2175deff3baf0fb
glastrm <>
2008-02-26 03:18:10 +0000

    Set lib dependencies up for when building libraries

--- fitsGenLib.py
+++ fitsGenLib.py
@@ -1,5 +1,7 @@
+#$Id$
 def generate(env, **kw):
-    env.Tool('addLibrary', library = ['fitsGen'], package = 'fitsGen')
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['fitsGen'])
     env.Tool('facilitiesLib')
     env.Tool('tipLib')
     env.Tool('astroLib')
