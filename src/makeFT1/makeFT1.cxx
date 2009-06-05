8c57238e21f91f8222604e031d7a6a7d86102d92
jchiang <>
2009-06-05 19:52:40 +0000

    allow users to set filename for temporary root file when reading merit data

--- src/makeFT1/makeFT1.cxx
+++ src/makeFT1/makeFT1.cxx
@@ -3,7 +3,7 @@
  * @brief Convert merit ntuple to FT1 format.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/src/makeFT1/makeFT1.cxx,v 1.41 2008/07/11 22:04:27 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/fitsGen/src/makeFT1/makeFT1.cxx,v 1.42 2009/03/12 04:12:08 jchiang Exp $
  */
 
 #include <cctype>
@@ -34,6 +34,8 @@
 #include "st_app/AppParGroup.h"
 #include "st_app/StApp.h"
 #include "st_app/StAppFactory.h"
+
+#include "tip/IFileSvc.h"
 
 #include "fitsGen/Ft1File.h"
 #include "fitsGen/MeritFile.h"
@@ -178,6 +180,7 @@ void MakeFt1::run() {
    m_pars.Prompt();
    m_pars.Save();
    std::string rootFile = m_pars["rootFile"];
+   std::string tempRootFile = m_pars["tempRootFile"];
    std::string fitsFile = m_pars["fitsFile"];
    std::string eventClassifier = m_pars["event_classifier"];
    std::string defaultFilter = m_pars["TCuts"];
@@ -216,6 +219,7 @@ void MakeFt1::run() {
    dataSubselector::Cuts my_cuts;
    fitsGen::Ft1File ft1(fitsFile, 0);
    try {
+      tip::IFileSvc::instance().setTmpFileName(tempRootFile);
       fitsGen::MeritFile merit(rootFile, "MeritTuple", filter);
       if (tstart != 0 || tstop != 0) {
          merit.setStartStop(tstart, tstop);
