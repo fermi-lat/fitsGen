1e9671b4dba3ffb8d564dbd3e19f31a46564fe8a
jchiang <>
2010-07-21 04:43:13 +0000

    remove applications and any dependence on st_app, st_graph, hoops, etc.

--- fitsGenLib.py
+++ fitsGenLib.py
@@ -1,16 +1,13 @@
-#$Id: fitsGenLib.py,v 1.2 2008/02/26 03:18:10 glastrm Exp $
+#$Id: fitsGenLib.py,v 1.3 2010/06/16 22:43:42 jchiang Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['fitsGen'])
     env.Tool('facilitiesLib')
     env.Tool('tipLib')
     env.Tool('astroLib')
-    env.Tool('st_facilitiesLib')
-    env.Tool('dataSubselectorLib')
-    env.Tool('st_appLib')
-    env.Tool('st_streamLib')
     env.Tool('embed_pythonLib')
     env.Tool('evtUtilsLib')
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
 
 def exists(env):
     return 1
