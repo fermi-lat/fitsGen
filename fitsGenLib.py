8d29296f8d0ae638d31eafb78303f0aa5f3064b7
jrb <>
2012-08-18 00:26:21 +0000

    support no-install headers on Windows

--- fitsGenLib.py
+++ fitsGenLib.py
@@ -1,13 +1,25 @@
-#$Id: fitsGenLib.py,v 1.3 2010/06/16 22:43:42 jchiang Exp $
+#$Id: fitsGenLib.py,v 1.4 2010/07/21 04:43:12 jchiang Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['fitsGen'])
+	if env['PLATFORM'] == 'win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'fitsGen') 
+	    env.Tool('findPkgPath', package = 'embed_python') 
+            env.Tool('findPkgPath', package = 'tip') 
+            env.Tool('findPkgPath', package = 'astro') 
+            env.Tool('findPkgPath', package = 'facilities') 
     env.Tool('facilitiesLib')
     env.Tool('tipLib')
     env.Tool('astroLib')
     env.Tool('embed_pythonLib')
     env.Tool('evtUtilsLib')
     env.Tool('addLibrary', library = env['rootGuiLibs'])
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'embed_python') 
+        env.Tool('findPkgPath', package = 'tip') 
+        env.Tool('findPkgPath', package = 'astro') 
+        env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('findPkgPath', package = 'evtUtils') 
 
 def exists(env):
     return 1
