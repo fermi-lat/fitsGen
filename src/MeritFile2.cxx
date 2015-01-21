fd670db5580e3637c16fc37659438a6765103d72
jchiang <>
2015-01-21 20:43:29 +0000

    enable array values in the merit tuple to be accessed by index

--- src/MeritFile2.cxx
+++ src/MeritFile2.cxx
@@ -3,11 +3,14 @@
  * @brief Interface to merit files that uses ROOT directly.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/fitsGen/src/MeritFile2.cxx,v 1.9 2012/10/29 05:23:17 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/fitsGen/src/MeritFile2.cxx,v 1.10 2013/03/28 05:41:15 jchiang Exp $
  */
+
+#include <cstdlib>
 
 #include <iostream>
 #include <sstream>
+#include <string>
 #include <stdexcept>
 
 #include "TChain.h"
@@ -15,6 +18,7 @@
 #include "TEventList.h"
 #include "TFile.h"
 #include "TLeaf.h"
+#include "TObjArray.h"
 #include "TSystem.h"
 #include "TTree.h"
 
@@ -141,15 +145,53 @@ void MeritFile2::setEntry(Long64_t index) {
 }
 
 double MeritFile2::operator[](const std::string & fieldname) {
-   BranchMap_t::iterator it = m_branches.find(fieldname);
-   if (it == m_branches.end()) {
-      BranchData_t branch_data(get_branch_pointer(fieldname));
-      m_tree->SetBranchAddress(fieldname.c_str(), branch_data.first);
-      m_branches[fieldname] = branch_data;
-      setEntry();
-      return recast_as_double(branch_data);
+   std::string branch_name(fieldname);
+   std::string::size_type pos;
+   int offset(0);
+   if ((pos=fieldname.find_first_of("[")) != std::string::npos) {
+      // Array is being accessed, so cast the substring in the square
+      // brackets to get the element number desired.
+      std::string my_substr(fieldname.substr(pos));
+      std::string::size_type end_pos(my_substr.find_first_of("]"));
+      if (end_pos == std::string::npos) {
+         throw std::runtime_error("Badly formed column name for merit access: "
+                                  + fieldname);
+      }
+      offset = std::atoi(my_substr.substr(1, end_pos).c_str());
+      // Get the recognized branch name for arrays from the leaf name.
+      branch_name = branchName(fieldname.substr(0, pos));
    }
-   return recast_as_double(it->second);
+   BranchMap_t::iterator it = m_branches.find(branch_name);
+   if (it == m_branches.end()) {
+      BranchData_t branch_data(get_branch_pointer(branch_name));
+      m_tree->SetBranchAddress(branch_name.c_str(), branch_data.first);
+      m_branches[branch_name] = branch_data;
+      setEntry();
+      return recast_as_double(branch_data, offset);
+   }
+   return recast_as_double(it->second, offset);
+}
+
+const std::string & MeritFile2::
+branchName(const std::string & truncated_fieldname) {
+   std::map<std::string, std::string>::const_iterator 
+      it(m_branchNames.find(truncated_fieldname));
+   if (it != m_branchNames.end()) {
+      return it->second;
+   }
+   // Find the desired branch name from the TTree and save it.
+   TObjArray * branch_list = m_tree->GetListOfBranches();
+   Long64_t nbranches = branch_list->GetEntries();
+   std::string branchname;
+   for (Long64_t i(0); i < nbranches; i++) {
+      branchname = branch_list->At(i)->GetName();
+      if (branchname.substr(0, truncated_fieldname.size()) 
+          == truncated_fieldname) {
+         m_branchNames[truncated_fieldname] = branchname;
+         break;
+      }
+   }
+   return m_branchNames.find(truncated_fieldname)->second;
 }
 
 short int MeritFile2::conversionType() const {
@@ -163,9 +205,15 @@ short int MeritFile2::conversionType() const {
 //BranchData_t MeritFile2::
 std::pair<void *, std::string> MeritFile2::
 get_branch_pointer(const std::string & fieldname) const {
-   TLeaf * leaf = m_tree->GetLeaf(fieldname.c_str());
+   std::string leaf_name(fieldname);
+   std::string::size_type pos;
+   if ((pos=fieldname.find_first_of('[')) != std::string::npos) {
+      leaf_name = fieldname.substr(0, pos);
+   }
+      
+   TLeaf * leaf = m_tree->GetLeaf(leaf_name.c_str());
    if (!leaf) {
-      throw std::runtime_error("leaf " + fieldname + " not found.");
+      throw std::runtime_error("leaf " + leaf_name + " not found.");
    }
    void * pointer(0);
    std::string type(leaf->GetTypeName());
@@ -185,25 +233,26 @@ get_branch_pointer(const std::string & fieldname) const {
    return std::make_pair(pointer, type);
 }
 
-double MeritFile2::recast_as_double(const BranchData_t & branch_data) const {
+double MeritFile2::
+recast_as_double(const BranchData_t & branch_data, int offset) const {
    if (branch_data.second == "Double_t") {
-      return static_cast<double>(*reinterpret_cast<Double_t *>
-                                 (branch_data.first));
+      return static_cast<double>(*(reinterpret_cast<Double_t *>
+                                   (branch_data.first) + offset));
    } else if (branch_data.second == "Float_t") {
-      return static_cast<double>(*reinterpret_cast<Float_t *>
-                                 (branch_data.first));
+      return static_cast<double>(*(reinterpret_cast<Float_t *>
+                                   (branch_data.first) + offset));
    } else if (branch_data.second == "Int_t") {
-      return static_cast<double>(*reinterpret_cast<Int_t *>
-                                 (branch_data.first));
+      return static_cast<double>(*(reinterpret_cast<Int_t *>
+                                   (branch_data.first) + offset));
    } else if (branch_data.second == "UInt_t") {
-      return static_cast<double>(*reinterpret_cast<UInt_t *>
-                                 (branch_data.first));
+      return static_cast<double>(*(reinterpret_cast<UInt_t *>
+                                   (branch_data.first) + offset));
    } else if (branch_data.second == "Long_t") {
-      return static_cast<double>(*reinterpret_cast<Long_t *>
-                                 (branch_data.first));
+      return static_cast<double>(*(reinterpret_cast<Long_t *>
+                                   (branch_data.first) + offset));
    } else if (branch_data.second == "ULong_t") {
-      return static_cast<double>(*reinterpret_cast<ULong_t *>
-                                 (branch_data.first));
+      return static_cast<double>(*(reinterpret_cast<ULong_t *>
+                                   (branch_data.first) + offset));
    }
 }
 
