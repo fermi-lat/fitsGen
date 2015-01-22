5cc34cd7b11df90a82aea578ba3ceaba4cfa1dbe
jchiang <>
2015-01-22 00:56:20 +0000

    increase allocation for long TCuts

--- src/MeritFile2.cxx
+++ src/MeritFile2.cxx
@@ -3,7 +3,7 @@
  * @brief Interface to merit files that uses ROOT directly.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/fitsGen/src/MeritFile2.cxx,v 1.10 2013/03/28 05:41:15 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/fitsGen/src/MeritFile2.cxx,v 1.11 2015/01/21 20:43:29 jchiang Exp $
  */
 
 #include <cstdlib>
@@ -17,6 +17,7 @@
 #include "TError.h"
 #include "TEventList.h"
 #include "TFile.h"
+#include "TFormula.h"
 #include "TLeaf.h"
 #include "TObjArray.h"
 #include "TSystem.h"
@@ -35,6 +36,8 @@ MeritFile2::MeritFile2(const std::string & meritfile,
    long root_err_level = gErrorIgnoreLevel;
    m_file = TFile::Open(meritfile.c_str());
    gErrorIgnoreLevel = root_err_level;
+
+   TFormula::SetMaxima(2000, 2000, 2000);
 
    if (m_file == 0) {
       throw std::runtime_error("Failed to load " + meritfile);
