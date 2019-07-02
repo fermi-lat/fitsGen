8bbdd15a1bc4aa7d1c8e59958ddbaf978d4ce31e
Alex Reustle <areustledev@gmail.com>
2019-07-02 12:56:11 -0400

    Explicitly use ROOT::v5::TFormula

--- src/MeritFile2.cxx
+++ src/MeritFile2.cxx
@@ -18,7 +18,7 @@
 #include "TError.h"
 #include "TEventList.h"
 #include "TFile.h"
-#include "TFormula.h"
+#include "v5/TFormula.h"
 #include "TLeaf.h"
 #include "TObjArray.h"
 #include "TSystem.h"
@@ -38,7 +38,7 @@ MeritFile2::MeritFile2(const std::string & meritfile,
    m_file = TFile::Open(meritfile.c_str());
    gErrorIgnoreLevel = root_err_level;
 
-   TFormula::SetMaxima(2000, 2000, 2000);
+   ROOT::v5::TFormula::SetMaxima(2000, 2000, 2000);
 
    if (m_file == 0) {
       throw std::runtime_error("Failed to load " + meritfile);
